1258298f30fa9a2f3e34d38ed5423b8b2db67fea
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-11-06 13:09:19 -0500

    created models and script folders and setup sample user chema and user seed file to test mongo database after depolyment.

---
+++ models/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    User: require("./User"),
+    // Resource: require("./Resource"),
+    // Feedback: require("./Feedback"),
+  };
+  
---
+++ scripts/seedUserDB.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/link-dyscovery"
+);
+
+const userSeed = [
+  {
+    emailAddress: "sample@gamil.com",
+    password: "password1234",
+    firstName: "Sam",
+    lastName: "Ple",
+    age: 40,
+    gender: "Male",
+    location: "Atlanta, GA",
+    bio: "I am a sample person.",
+    learningDisabilities: "None",
+  },
+  {
+    emailAddress: "anotherperson@gamil.com",
+    password: "secretpassword",
+    firstName: "Another",
+    lastName: "Person",
+    age: 18,
+    gender: "Female",
+    location: "Atlanta, GA",
+    bio: "I am another sample person.",
+    learningDisabilities: "None",
+  },
+];
+
+db.User.deleteMany({})
+  .then(() => db.Workout.collection.insertMany(userSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });

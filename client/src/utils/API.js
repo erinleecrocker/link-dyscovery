3ce9f3f3d64490cef0597f3ae583adf0ba76f0a0
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-11-10 10:58:44 -0500

    setup router in the app.js and brought in all current pages. Inserted navbar into the app.js and tested the home connection.

---
+++ client/src/utils/API.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export default {
+  // Gets all users
+  getUsers: function() {
+    return axios.get("/api/user");
+  },
+  // Gets the user with the given id
+  getUser: function(id) {
+    return axios.get("/api/user/" + id);
+  },
+  // Deletes the user with the given id
+  deleteUser: function(id) {
+    return axios.delete("/api/user/" + id);
+  },
+// Edit the user with the given id
+  editUser: function(id) {
+      return axios.put("/api/user/" + id)
+  },
+  // Saves a user to the database
+  saveUser: function(userData) {
+    return axios.post("/api/user", userData);
+  }
+};

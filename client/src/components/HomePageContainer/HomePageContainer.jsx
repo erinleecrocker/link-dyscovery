9926a986a2ef841e868089fa10f9055ee7caed47
Erin Crocker <erindowns1@me.com>
2020-11-12 14:39:47 -0500

    styled resource card home button and home page

--- client/src/components/HomePageContainer/HomePageContainer.jsx
+++ client/src/components/HomePageContainer/HomePageContainer.jsx
@@ -1,46 +1,45 @@
 import React from "react";
 import HomeButton from "../HomeButton/HomeButton";
-import './HomePageContainer.css';
+import "./HomePageContainer.css";
 import SiteTitle from "../SiteTitle/SiteTitle";
 
 const HomePageContainer = () => {
   return (
     <div className="container">
-      <div className="row ml-4">
-      <SiteTitle/>
+      <div className="row">
+        <div className="col-md-12">
+        <SiteTitle />
+        </div>
       </div>
-        < div className="card" id="home-container">
-          <div className="card-body ">
-            <p className="card-text home-card-text">
-              Welcome to Link Dyscovery! This application is a place for adults
-              living with learning disabilities to find, rate, and share
-              resources in a convenient and safe place with other like minded
-              adults. To get started, select the “Create an Account” button.
-              Don’t want to create an account? That’s ok! You won’t have full
-              access to all the resources but you can still view the verified
-              resources page. By creating an account you will have the ability
-              to rate and share resources with the community as well as have
-              access to the community shared resources.
-            </p>
-            <div className="row justify-content-md-center">
-              <HomeButton 
-                title="View Resources"
-                link="/resource-category"
-               
-              />
-              <HomeButton
-                title="Login"
-                link="/login"
-              
-              />
-              <HomeButton 
-                title="Create an Account"
-                link="/create-account"
-                
-              />
+      <div className="card" id="home-container">
+        <div className="card-body">
+          <div className="row mb-4">
+            <div className="col-md-12">
+              <p className="card-text home-card-text">
+                Welcome to Link Dyscovery! This application is a place for
+                adults living with learning disabilities to find, rate, and
+                share resources in a convenient and safe place with other like
+                minded adults. To get started, select the “Create an Account”
+                button. Don’t want to create an account? That’s ok! You won’t
+                have full access to all the resources but you can still view the
+                verified resources page. By creating an account you will have
+                the ability to rate and share resources with the community as
+                well as have access to the community shared resources.
+              </p>
             </div>
-
           </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="row justify-content-center">
+                <HomeButton title="View Resources" link="/resource-category" />
+
+                <HomeButton title="Login" link="/login" />
+
+                <HomeButton title="Create an Account" link="/create-account" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- client/src/components/SiteTitle/SiteTitle.jsx
+++ client/src/components/SiteTitle/SiteTitle.jsx
@@ -4,19 +4,14 @@ import './SiteTitle.css';
 
 const SiteTitle = () => {
   return (
-    <div>
-      <div className="row">
-      <div className="d-flex">
-        <div className="col-md">
+      <div className="row d-md-flex">
+        <div className="col-md-6">
           <img id="main-img" src={MainLogo} />
         </div>
-
-        <div className="col-md align-self-end">
+        <div className="col-md-6 align-self-lg-end">
           <p id="home-title-tag">Communicating Differently Together</p>
         </div>
       </div>
-      </div>
-    </div>
   );
 };
 

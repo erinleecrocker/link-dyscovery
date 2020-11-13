b7e9ced271c48c6ad3743d2f5f863fab1fed86f0
Erin Crocker <erindowns1@me.com>
2020-11-13 16:41:32 -0500

    rearranged profile page to be more ballanced

--- client/src/components/ProfileBody/ProfileBody.jsx
+++ client/src/components/ProfileBody/ProfileBody.jsx
@@ -1,20 +1,20 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ProfileChanges from "../ProfileChanges/ProfileChanges";
 import ProfilePlaceholder from "../../images/profilePlaceholder.png";
 
 import "./ProfileBody.css";
 
 const ProfileBody = (props) => {
-
-  const [modalShow, setModalShow] = React.useState(true);
+  const [modalShow, setModalShow] = React.useState(false);
 
   return (
     <div className="container">
+      <div className="row">
       <div className="card">
         <div className="card-body defaultStyle">
           <div className="col-md-12">
             <div className="row">
-              <div className="col-md-3">
+              <div className="col-md-3 mr-2">
                 <div className="row">
                   <div className="col-md-12">
                     <img
@@ -23,29 +23,21 @@ const ProfileBody = (props) => {
                       alt="placeholder"
                     />
                   </div>
-                  <form action="/action_page.php">
+                  <div className="row">
+                    <div className="col-md-12 ml-4 mb-3">
+                    <form action="/action_page.php">
                     <label htmlFor="img">Select image:</label>
                     <input type="file" id="img" name="img" accept="image/*" />
                     <input type="submit" />
                   </form>
-                </div>
-                <div className="row ml-2 mb-4">
-                  <div className="col-md-12">
-                    <div className="row">
-                      <div className="col-md-12 input-labels-bold">
-                        User's Resources
-                      </div>
                     </div>
-                    <div className="row">
-                      <div className="col-md-12 input-labels-bold">
-                        User's Saved Resources
-                      </div>
-                    </div>
+                  
                   </div>
+                 
                 </div>
               </div>
               <div className="col-md-4 input-labels">
-                <div className="card mb-4" id="profile-card">
+                <div className="card mb-4 profile-card">
                   <div className="row">
                     <div className="col-md-12">
                       <h6> Username: {props.userName}</h6>
@@ -58,7 +50,12 @@ const ProfileBody = (props) => {
                   </div>
                   <div className="row">
                     <div className="col-md-12">
-                      <button className="btn" id="profile-button" variant="primary" onClick={() => setModalShow(true)}>
+                      <button
+                        className="btn"
+                        id="profile-button"
+                        variant="primary"
+                        onClick={() => setModalShow(true)}
+                      >
                         Edit Profile
                       </button>
                       <button
@@ -75,6 +72,7 @@ const ProfileBody = (props) => {
               <div className="col-md-4">
                 <div className="row">
                   <div className="col-md-12">
+                  <div className="card profile-card">
                     <div className="row">
                       <div className="col-md-12">
                         <h6 className="input-labels-bold">About Me:</h6>
@@ -83,6 +81,21 @@ const ProfileBody = (props) => {
                     <div className="row ml-1">
                       <div className="col-md-12 input-labels">
                         <p>{props.bio}</p>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+                  <div className="row mb-4 mt-4 ml-4">
+                    <div className="col-md-12">
+                      <div className="row">
+                        <div className="col-md-12 input-labels-bold">
+                          User's Resources
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-md-12 input-labels-bold">
+                          User's Saved Resources
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -112,8 +125,8 @@ const ProfileBody = (props) => {
           // Location = {props.location}
         />
       </div>
+      </div>
     </div>
-    
   );
 };
 

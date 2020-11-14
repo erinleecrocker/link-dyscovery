c332ed94b96c77b90470673176edb6eb7d9aa117
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-11-13 19:04:01 -0500

    fixing errors and warnings

--- server.js
+++ server.js
@@ -52,6 +52,10 @@ app.use(AuthController)
   app.use("/api/resource", ResourceController);
   app.use("/api/feedback", FeedbackController);
 
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  // });
+  
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
